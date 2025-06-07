42aac65725a09bc76a2b30bfd2020d907d0e4036
VascoBrito <vascolealbrito@gmail.com>
2025-06-07 13:38:16 +0100

    conditional rendering updates

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,159 +1,13 @@
 "use client";
 
-import Image from "next/image";
 import { useEffect, useState } from "react";
-
-// export default function Home() {
-//   const [tracks, setTracks] = useState([]);
-//   const [input, setInput] = useState("");
-//   const [loading, setLoading] = useState(false);
-
-//   const handleSubmit = async () => {
-//     setLoading(true);
-//     const res = await fetch("/api/extract", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({ artists: input }),
-//     });
-
-//     await loadTracks();
-//     const result = await res.json();
-//     setLoading(false);
-//     alert(result.message || "Erro ao extrair dados");
-//   };
-
-//   const loadTracks = async () => {
-//     fetch("/api/tracks")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log("🔍 Dados recebidos:", data[0]);
-//         setTracks(data);
-//       });
-//   };
-//   useEffect(() => {
-//     loadTracks();
-//   }, []);
-
-//   const artistNames = [...new Set(tracks.map((t) => t.artist_name))];
-//   const artistDisplay = artistNames.join(", ");
-
-//   const data_lenght = tracks.length;
-
-//   const getTrackStats = (tracks) => {
-//     if (!tracks || tracks.length === 0) return null;
-
-//     const durations = tracks
-//       .map((t) => parseFloat(t.duration_formatted))
-//       .filter(Boolean);
-
-//     const total = durations.length;
-//     const sum = durations.reduce((acc, val) => acc + val, 0);
-//     const average = (sum / total).toFixed(2);
-//     const min = Math.min(...durations).toFixed(2);
-//     const max = Math.max(...durations).toFixed(2);
-
-//     return { total, average, min, max };
-//   };
-
-//   const getShortestTrack = (tracks) => {
-//     if (!tracks || tracks.length === 0) return null;
-
-//     return tracks.reduce((minTrack, current) => {
-//       const currentDuration = parseFloat(current.duration_min || "0");
-//       const minDuration = parseFloat(minTrack.duration_min || "0");
-
-//       return currentDuration < minDuration ? current : minTrack;
-//     });
-//   };
-//   const shortest = getShortestTrack(tracks);
-
-//   const stats = getTrackStats(tracks);
-
-//   return (
-//     <main>
-//       <div className="p-6 ">
-//         <h1 className="text-2xl font-bold mb-4">Buscar dados de artistas</h1>
-//         <div className="flex gap-2 items-center">
-//           <input
-//             className="border p-2 mr-2 w-96 text-black"
-//             placeholder="Mariza, Slow J, Carolina Deslandes..."
-//             value={input}
-//             onChange={(e) => setInput(e.target.value)}
-//           />
-
-//           <button
-//             onClick={handleSubmit}
-//             className="bg-blue-500 text-white px-4 py-2 rounded"
-//           >
-//             {loading ? "Buscando..." : "Extrair"}
-//           </button>
-
-//           <dropdown> </dropdown>
-
-//           <p className=" ml-12 font-bold "> Músicas: {data_lenght}</p>
-//         </div>
-//       </div>
-//       {stats && (
-//         <div className="mt-8 p-4 border rounded-lg shadow  text-black bg-white w-fit mx-auto">
-//           <h2 className="text-xl  font-bold mb-2">
-//             📊 Estatísticas das Faixas
-//           </h2>
-//           <p>
-//             Total de faixas: <strong>{stats.total}</strong>
-//           </p>
-//           <p>
-//             Média de duração: <strong>{stats.average} min</strong>
-//           </p>
-
-//           {shortest && (
-//             <p>
-//               🎧 Faixa mais curta: <strong>{shortest.track_name}</strong> (
-//               {shortest.duration_formatted})
-//             </p>
-//           )}
-//           <p>
-//             Duração mínima: <strong>{stats.min} min</strong>
-//           </p>
-//           <p>
-//             Duração máxima: <strong>{stats.max} min</strong>
-//           </p>
-//         </div>
-//       )}
-
-//       <div className="grid grid-flow-row p-6">
-//         <h1 className="text-2xl m-auto w-fit font-bold mb-12 capitalize ">
-//           Faixas Extraídas de {artistDisplay}
-//         </h1>
-
-//         <div className="table-auto w-[70%] justify-center items-center flex flex-wrap m-auto text-sm gap-y-11">
-//           <div className="flex flex-wrap justify-center">
-//             {tracks.map((track, i) => (
-//               <span key={i} className="flex flex-col gap-4  m-4 max-w-[12rem]">
-//                 <div>
-//                   <img
-//                     className=" w-full aspect-square rounded-full shadow-md hover:scale-110 hover:shadow-white transition-all cursor-pointer"
-//                     src={track.album_image_url}
-//                   />
-//                 </div>
-//                 <div className="items-end justify-end">
-//                   <p className="p-2">{track.album_name}</p>
-//                   <p className="p-2">{track.track_name}</p>
-//                   <p className="p-2">{track.release_date}</p>
-//                   <p className="p-2">{track.duration_formatted}</p>
-//                 </div>
-//               </span>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     </main>
-//   );
-// }
+import { Spinner } from "@/components/Spinner";
 
 const GENRES = ["Pop", "Hip-hop", "Rock", "Fado", "jazz"];
 
 export default function Home() {
-  const [genre, setGenre] = useState("pop");
+  const [genre, setGenre] = useState("");
+
   const [tracks, setTracks] = useState([]);
   const [loading, setLoading] = useState(false);
 
@@ -204,10 +58,16 @@ export default function Home() {
       className=" mt-12 p-6 
      justify-center h-screen "
     >
-      <div className="flex flex-col justify-start  w-full">
-        <h1 className="text-3xl text-center font-bold mb-4 uppercase">
-          Top 10 - {genre}
-        </h1>
+      <div className="flex flex-col justify-start p-4  w-full text-white">
+        {genre ? (
+          <h1 className="text-3xl text-center font-bold mb-4 uppercase">
+            Top 10 - {genre}
+          </h1>
+        ) : (
+          <h1 className="text-3xl text-center font-bold mb-4 uppercase">
+            Spotify Data Explorer
+          </h1>
+        )}
         <div className="text-black p-2 text-center ">
           <select
             value={genre}
@@ -216,8 +76,11 @@ export default function Home() {
               setGenre(selected);
               fetchAndLoadGenre(selected);
             }}
-            className="w-[12rem] p-2 text-xl text-center"
+            className="min-w-[12rem] p-2 px-4 text-xl text-center rounded-md"
           >
+            <option value="" disabled hidden>
+              Seleciona o TOP 10
+            </option>
             {GENRES.map((g) => (
               <option key={g} value={g}>
                 {g.charAt(0).toUpperCase() + g.slice(1)}
@@ -227,18 +90,22 @@ export default function Home() {
         </div>
       </div>
 
-      {!loading ? (
-        Array.isArray(tracks) && tracks.length > 0 ? (
-          <div className="w-full p-4 grid justify-center items-center h-full">
-            <div className="relative flex flex-wrap mt-6 justify-center gap-8 items-center">
+      {genre ? (
+        loading ? (
+          <div className="h-screen w-full p-4 grid justify-center items-center ">
+            <Spinner className="text-white"></Spinner>
+          </div>
+        ) : tracks.length > 0 ? (
+          <div className="w-[80%] m-auto p-4 grid justify-center items-start h-full">
+            <div className="relative flex flex-wrap mt-12 justify-center gap-8 items-center">
               {tracks.map((track: any, i: number) => (
                 <a
+                  key={i}
                   href={track.spotify_url}
                   target="_blank"
-                  className="group overflow-hidden shadow-sm shadow-white rounded-xl w-[16rem] h-[16rem]  cursor-pointer hover:shadow-none "
-                  key={i}
+                  className="group overflow-hidden rounded-xl w-[16rem] h-[16rem] cursor-pointer hover:shadow-none"
                 >
-                  <div className="relative  flex flex-col text-black transition-all  ">
+                  <div className="relative flex flex-col text-black transition-all">
                     <img
                       src={track.album_cover_url}
                       alt={track.artist_name}
@@ -246,10 +113,10 @@ export default function Home() {
                     />
                     <div className="bg-black bg-opacity-90 w-full h-full absolute flex flex-col justify-center items-center gap-2 pt-2 p-4 translate-y-[200%] group-hover:translate-y-[0%] transition-all text-white">
                       <h2 className="text-2xl">{track.artist_name}</h2>
-                      <p className=""> {track.followers} Seguidores</p>
-                      <p className="">{track.genre}</p>
+                      <p>{track.followers} Seguidores</p>
+                      <p>{track.genre}</p>
                       <p className="text-center">{track.track_name}</p>
-                      <p className="">{track.release_date}</p>
+                      <p>{track.release_date}</p>
                     </div>
                   </div>
                 </a>
@@ -257,17 +124,17 @@ export default function Home() {
             </div>
           </div>
         ) : (
-          <div className="h-full grid justify-center items-center">
-            <p className="text-center">
-              <span className="loading loading-dots loading-xl"></span>
-            </p>
+          <div className="h-screen w-full p-4 grid justify-center items-center ">
+            <Spinner className="text-white">
+              <span className="text-white pt-4">
+                A carregar TOP 10 músicas de {genre}{" "}
+              </span>
+            </Spinner>
           </div>
         )
       ) : (
-        <div className="h-full grid justify-center items-center">
-          <p className="text-center">
-            <span className="loading loading-dots loading-xl"></span>
-          </p>
+        <div className=" h-full w-full grid justify-center items-center p-4 rounded text-white">
+          Nenhum gênero selecionado.
         </div>
       )}
     </div>
--- src/etl.py
+++ src/etl.py
@@ -7,12 +7,9 @@ from pathlib import Path
 from datetime import datetime
 from spotify_api import get_token, get_top_tracks, search_artists_by_genre
 
-# At the top of the file (after imports):
 ROOT_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = ROOT_DIR / "data"
 os.makedirs(DATA_DIR, exist_ok=True)
-
-# src/etl.py
 
 def fetch_top10_by_genre(genre):
     token = get_token()
@@ -84,91 +81,3 @@ if __name__ == "__main__":
         sys.exit(1)
     genre = sys.argv[1]
     fetch_top10_by_genre(genre)
-
-
-# def fetch_artists_from_input():
-#     token = get_token()
-#     print("Digite nomes de artistas separados por vírgula:")
-#     input_str = sys.argv[1]  # pega o argumento passado pelo terminal
-#     artist_names = [name.strip() for name in input_str.split(",")]
-
-#     artist_data = []
-#     for name in artist_names:
-#         artist_id = get_artist_id(name, token)
-#         if artist_id:
-#             print(f"{name} → {artist_id}")
-#             artist_data.append({"name": name, "id": artist_id})
-#         else:
-#             print(f"❌ {name} não encontrado.")
-
-#     df_artists = pd.DataFrame(artist_data)
-#     df_artists.to_csv(DATA_DIR / "artists.csv", index=False)
-#     return df_artists, token
-
-# def fetch_albums(df_artists, token):
-#     albums_data = []
-#     for _, row in df_artists.iterrows():
-#         artist_name = row['name']
-#         artist_id = row['id']
-#         print(f"📀 Buscando álbuns de {artist_name}")
-#         albums = get_artist_albums(artist_id, token)
-#         for album in albums:
-#             albums_data.append({
-#                 "artist_name": artist_name,
-#                 "artist_id": artist_id,
-#                 "album_id": album["id"],
-#                 "album_name": album["name"],
-#                 "release_date": album["release_date"],
-#                 "total_tracks": album["total_tracks"],
-#                 "album_type": album["album_type"],
-#                 "album_image_url" : album["images"] [0]["url"] if album["images"] else None            
-#                 })
-
-#     df_albums = pd.DataFrame(albums_data).drop_duplicates(subset="album_id")
-#     df_albums.to_csv(DATA_DIR / "albums.csv", index=False)
-#     return df_albums
-
-# def fetch_tracks(df_albums, token):
-#     tracks_data = []
-#     for _, row in df_albums.iterrows():
-#         artist_name = row["artist_name"]
-#         album_name = row["album_name"]
-#         album_id = row["album_id"]
-#         album_image_url = row.get("album_image_url")
-#         release_date= row["release_date"]
-
-#         print(f"🎶 Buscando faixas do álbum {album_name} ({artist_name})")
-
-#         try:
-#             tracks = get_album_tracks(album_id, token)
-#             for track in tracks:
-#                 ms = track["duration_ms"]
-#                 minutes = ms // 60000
-#                 seconds = (ms % 60000) // 1000
-#                 duration_min = round(ms / 60000, 2)
-#                 duration_formatted = f"{int(minutes)}:{int(seconds):02}"
-
-#                 tracks_data.append({
-#                     "artist_name": artist_name,
-#                     "album_name": album_name,
-#                     "album_id": album_id,
-#                     "release_date": release_date,
-#                     "track_name": track["name"],
-#                     "track_id": track["id"],
-#                     "track_number": track["track_number"],
-#                     "duration_formatted": duration_formatted,
-#                     "album_image_url": album_image_url
-#                 })
-#         except Exception as e:
-#           print(f"⚠️ Erro em {album_name}: {e}")
-
-#     df_tracks = pd.DataFrame(tracks_data).drop_duplicates(subset="track_id")
-#     df_tracks.to_csv(DATA_DIR / "tracks.csv", index=False)
-
-# def main():
-#     df_artists, token = fetch_artists_from_input()
-#     df_albums = fetch_albums(df_artists, token)
-#     fetch_tracks(df_albums, token)
-#     print("✅ Pipeline completa!")
-
-
